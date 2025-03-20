37ded1f50fa9cf455715b4911116eb5a3e0095a7
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:03:56 +0530

    making retaurant bill

---
+++ Java Script Practise/NumberAndString/RestaurantBill.js
@@ -0,0 +1,10 @@
+console.log('Welcome to My Restaurant..!!');
+console.log("Our Menu: Dal, Roti, Icecream");
+console.log("What will you Want to choose?");
+let dal, Roti, Icecream;
+
+dal = 100;
+Roti = 10;
+Icecream = 30;
+console.log("User Choose 1 plate daal, 2 piece Roti, Icecream 1 bowl");
+console.log(`Total Price: ${dal+(2*Roti)+Icecream}`);
