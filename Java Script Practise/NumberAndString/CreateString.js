e6700f4245e537f32759b09efca225394896c985
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:21:28 +0530

    using both type of strings

---
+++ Java Script Practise/NumberAndString/CreateString.js
@@ -0,0 +1,9 @@
+// Create strings using all 3 methods.
+
+// Double quotes Method, SIngle quotes method, Using Backticks
+str1 ="Nitish Dwivedi";
+str2 ='Nitish Dwivedi';
+str3 =`Nitish Dwivedi`;
+console.log(`Double quotes : ${str1}, Single Quotes : ${str2}, Using Backtick: ${str3}`);
+
+
