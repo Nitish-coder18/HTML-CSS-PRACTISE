42651a3158d9199d12b84c648204a277bae46042
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:44:56 +0530

    i will do it some time after

---
+++ Java Script Practise/3 If_Alternative/TicketBookingAges.js
@@ -0,0 +1,6 @@
+/*Nested If Statement: Write code to determine the ticket price for a movie. The function should consider the following:
+ • If the viewer is under 13, the ticket is free. 
+ • If the viewer is between 13 and 60, check if it's a weekend. If yes, the ticket price is Rs 500; otherwise, it’s Rs 300. 
+• If the viewer is over 60, the ticket price is Rs 250.*/
+
+let age = prompt("What's Your Age: ");
