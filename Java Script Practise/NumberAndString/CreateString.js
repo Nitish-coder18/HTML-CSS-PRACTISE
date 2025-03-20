15238535a138892150e9a047381a5df88d8fd327
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:26:01 +0530

    concatenate of a number

--- Java Script Practise/NumberAndString/CreateString.js
+++ Java Script Practise/NumberAndString/CreateString.js
@@ -6,4 +6,6 @@ str2 ='Nitish Dwivedi';
 str3 =`Nitish Dwivedi`;
 console.log(`Double quotes : ${str1}, Single Quotes : ${str2}, Using Backtick: ${str3}`);
 
-
+// concatenate string with string and string with number
+console.log("Nitish Dwivedi" + "5");
+console.log("Nitish Dwivedi" - 5);
