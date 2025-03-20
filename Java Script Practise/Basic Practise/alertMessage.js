5756439bb174c0444e1f9c60ce218a303274199e
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 08:35:11 +0530

    Using Math calculate the  following:  => 75-25  => 3+3-5

--- Java Script Practise/Basic Practise/alertMessage.js
+++ Java Script Practise/Basic Practise/alertMessage.js
@@ -4,3 +4,10 @@ alert("Hello Ap Sabhi ko good morning..!!");
 
 // Display your name in a popup.
 alert("Nitish Dwivedi");
+
+// Using Math calculate the 
+// following:
+//  => 75-25
+//  => 3+3-5
+console.log(75-25)
+console.log(3+3-5);
