b1fd4f2c466e0b6037d11ba8ca061ddeb025c5d3
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:36:32 +0530

    done

---
+++ Java Script Practise/3 If_Alternative/NumberIsPosNegZer.js
@@ -0,0 +1,12 @@
+/*if-Else Statement: Write code that checks if a number is positive, negative, or zero. Use an `if-else` statement for this purpose*/
+
+let Num = prompt("Enter a Nummber: ");
+if (Num>0) {
+    console.log("Number is Positive..!!");
+}
+else if (Num<0) {
+    console.log("Number is Negative..!!");
+}
+else {
+    console.log("Number is Neither negavtive nor Positive. It's is zero..!!");
+}
