e937536b55a5b43f3cac3923397ccf4f4b97b1c9
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 09:15:26 +0530

    calculate GST

---
+++ Java Script Practise/NumberAndString/CalculateGST.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Apple Showroom..!!");
+console.log ("User choose: iPhone15 ₹79,990 and 2 Air pods Pro ₹24990 each");
+let iphone = 500;
+let AirPods = 1000;
+let TotalPrice = iphone+(2*AirPods);
+console.log(`Total Price: ${TotalPrice}`);
+console.log(`Calculate 18% GST: ${(TotalPrice*18)/100}`);
+
