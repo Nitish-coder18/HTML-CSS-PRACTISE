2764732a6b1471a228c4b9dd34dc46b269ba2653
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-20 08:30:52 +0530

    Display your name in a popup.

--- Java Script Practise/Basic Practise/alertMessage.js
+++ Java Script Practise/Basic Practise/alertMessage.js
@@ -1,3 +1,6 @@
 // console.log(`kya bolte ho bhai log shuru kiya jaye java script..!!`)
 // Use an alert to display Good Morning
 alert("Hello Ap Sabhi ko good morning..!!");
+
+// Display your name in a popup.
+alert("Nitish Dwivedi");
