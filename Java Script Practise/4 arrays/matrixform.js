e68ccc4192e3cfbfb27d3c5c86ea7c050edcd672
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-03-30 20:58:54 +0530

    array program in js

---
+++ Java Script Practise/4 arrays/matrixform.js
@@ -0,0 +1,8 @@
+// in this program i am going to create a matrix..!!
+console.log(`hii`);
+let arr = [1, 2, 3 , 4, 5, 6, 7, 8, 9];
+console.log(arr);
+
+for (i=arr[0]; i<=arr[2]; i++) {
+    console.log(i);
+}
