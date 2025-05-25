a19a240737c686e3738dbc93b678fc50af00e51d
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-05-25 14:52:11 +0530

    system setup for vite react

---
+++ REACT/first_project_vite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Nitish Dwivedi</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consectetur laudantium accusantium iusto expedita asperiores illo ipsam fugiat quo ducimus!</p>
+      <button>click me!</button>
+    </>
+  )
+}
+
+export default App
