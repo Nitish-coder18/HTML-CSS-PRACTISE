64af70ccca917d1a3d90a663d557269c4c0c6ebe
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-05-25 15:26:11 +0530

    updation of my first project

--- REACT/first_project_vite/src/App.jsx
+++ REACT/first_project_vite/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Heading from './components/Heading'
 
 function App() {
   
   return (
     <>
-      
+      <Heading />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consectetur laudantium accusantium iusto expedita asperiores illo ipsam fugiat quo ducimus!</p>
       <button>click me!</button>
     </>
--- REACT/first_project_vite/src/components/Heading.jsx
+++ REACT/first_project_vite/src/components/Heading.jsx
@@ -1,6 +1,7 @@
 // first i am going to declare the class method after that i am going with functions becuase it is easily understandable..!!
 
 import { Component } from "react";
+import './Heading.css';
 
 class Heading extends Component {
     render() {
