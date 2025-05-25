269e1ecb3003ebd61b56bf44af7d06787cd00e70
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-05-25 10:54:55 +0530

    largest of three just for practice

---
+++ Java Script Practise/1 Basic Practise/LargestOfThree.js
@@ -0,0 +1,13 @@
+let a, b, c;
+a =18;
+b =19;
+c =13;
+
+if (a>b && a>c) {
+    console.log("A is the Largest Number..!!");
+} else if (b>a && b>c) {
+    console.log("B is the largest Number..!!");
+}  else {
+    console.log("C is the Largest Number..!!");
+}
+
