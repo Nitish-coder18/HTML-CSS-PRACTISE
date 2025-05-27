fa865327895eafaa6588c8e7d66a49661e24b6cd
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-05-27 20:58:02 +0530

    update

--- REACT/first_project_vite/src/components/Paragraph.jsx
+++ REACT/first_project_vite/src/components/Paragraph.jsx
@@ -3,10 +3,19 @@
 // this is my first way to apply function method 
 import './paragraph.css'
 
-
+/*
 function Paragraph() {
     return <p className='
       para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consectetur laudantium accusantium iusto expedita asperiores illo ipsam fugiat quo ducimus!</p>;
 } 
+*/
+
+
+// try to short the same function using arrow methods
+
+const Paragraph = () => {
+    return <p className='
+      para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consectetur laudantium accusantium iusto expedita asperiores illo ipsam fugiat quo ducimus!</p>;
+}
 
 export default Paragraph;
