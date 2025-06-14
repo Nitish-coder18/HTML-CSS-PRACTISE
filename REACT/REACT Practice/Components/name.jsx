13e91dbf4854448b5ad87bb8546142b6e6188e30
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-06-14 13:59:30 +0530

    updates

---
+++ REACT/REACT Practice/Components/name.jsx
@@ -0,0 +1,7 @@
+import './name.css'
+
+function Name() {
+    return <h1 className='nam'>Hello World..!!</h1>;
+}
+
+export default Name;
---
+++ REACT/REACT Practice/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Name from '../Components/name'
+
+function App() {
+
+  return (
+    <>
+      <Name />
+    </>
+  )
+}
+
+export default App
