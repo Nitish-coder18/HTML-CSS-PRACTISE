c7846d4d7a93ec245eb77d9b394d0b5d4eed473d
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-06-17 21:26:23 +0530

    import app name

--- REACT/Todo-ui-bootstrap/src/App.jsx
+++ REACT/Todo-ui-bootstrap/src/App.jsx
@@ -1,12 +1,32 @@
 
 import './App.css'
+import TodoName from './components/Title'
 
 function App() {
   
 
   return (
     <>
-      <center><h1>Todo App</h1></center>
+      <center>
+        <TodoName />
+       
+        
+        <div class="container text-center">
+  <div class="row justify-content-md-center">
+  </div>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Enter Todo Here"></input>
+    </div>
+    <div class="col-md-auto">
+       <input type="Date" class="form-control"></input>
+    </div>
+    <div class="col col-lg-2">
+      <button type="button" class="btn btn-success">Success</button>
+    </div>
+  </div>
+</div>
+    </center>
     </>
   )
 }
