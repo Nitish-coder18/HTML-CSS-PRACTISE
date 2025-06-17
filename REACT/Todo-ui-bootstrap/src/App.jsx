fe8c99bda6c21dff9f1572576d02eb4f92d710b5
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-06-17 21:28:56 +0530

    enter data to todo list

--- REACT/Todo-ui-bootstrap/src/App.jsx
+++ REACT/Todo-ui-bootstrap/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import EnterTodo from './components/EnterTodo'
 import TodoName from './components/Title'
 
 function App() {
@@ -9,23 +10,9 @@ function App() {
     <>
       <center>
         <TodoName />
-       
+        <EnterTodo />
         
-        <div class="container text-center">
-  <div class="row justify-content-md-center">
-  </div>
-  <div class="row">
-    <div class="col">
-      <input type="text" class="form-control" placeholder="Enter Todo Here"></input>
-    </div>
-    <div class="col-md-auto">
-       <input type="Date" class="form-control"></input>
-    </div>
-    <div class="col col-lg-2">
-      <button type="button" class="btn btn-success">Success</button>
-    </div>
-  </div>
-</div>
+        
     </center>
     </>
   )
---
+++ REACT/Todo-ui-bootstrap/src/components/EnterTodo.jsx
@@ -0,0 +1,19 @@
+const EnterTodo = () => {
+    return <div class="container text-center">
+  <div class="row justify-content-md-center">
+  </div>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Enter Todo Here"></input>
+    </div>
+    <div class="col-md-auto">
+       <input type="Date" class="form-control"></input>
+    </div>
+    <div class="col col-lg-2">
+      <button type="button" class="btn btn-success">Success</button>
+    </div>
+  </div>
+</div>
+}
+
+export default EnterTodo;
