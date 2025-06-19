e242edf4ab7b34689c0c79dedcd3acc153a50650
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-06-19 22:38:28 +0530

    update todoitems.jsx

--- REACT/Todo-ui-bootstrap/src/App.jsx
+++ REACT/Todo-ui-bootstrap/src/App.jsx
@@ -2,26 +2,16 @@ import './App.css';
 import EnterTodo from './components/EnterTodo';
 import TodoName from './components/Title';
 import Todoitem from './components/TodoItem.jsx'; // This is the component
-
+import TodoItems from './components/TodoItems.jsx';
 function App() {
-  const todoItems = [
-    { id: 1, todoText: 'Buy Milk', todoDate: '17-Jun-2025' },
-    { id: 2, todoText: 'Go To College', todoDate: 'Every Day' },
-    { id: 3, todoText: 'Exercise', todoDate: '19-Jun-2025' }
-  ];
+  
 
   return (
     <>
       <center>
         <TodoName />
         <EnterTodo />
-        {todoItems.map((item) => (
-          <Todoitem
-            key={item.id}
-            todoText={item.todoText}
-            todoDate={item.todoDate}
-          />
-        ))}
+        <TodoItems  />
       </center>
     </>
   );
---
+++ REACT/Todo-ui-bootstrap/src/components/TodoItems.jsx
@@ -0,0 +1,23 @@
+import Todoitem from "./TodoItem";
+
+const TodoItems = () => {
+    const todoItems = [
+    { id: 1, todoText: 'Buy Milk', todoDate: '17-Jun-2025' },
+    { id: 2, todoText: 'Go To College', todoDate: 'weekday' },
+    { id: 3, todoText: 'Exercise', todoDate: '19-Jun-2025' }
+  ];
+  return (
+    <>
+    {todoItems.map((item) => (
+          <Todoitem
+            key={item.id}
+            todoText={item.todoText}
+            todoDate={item.todoDate}
+          />
+        ))}
+    </>
+  );
+};
+
+
+export default TodoItems;
