c2c6f38b5835c2eed6eb01d6ab90bcdacc978c09
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-10-05 22:30:07 +0530

    create airbnb projects and start working on it

---
+++ NODE & EXPRESS/1 Airbnb/app.js
@@ -0,0 +1,36 @@
+// External Module
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use((req, res, next) => {
+  console.log('Request Received', req.url, req.method, req.body);
+  next();
+});
+
+
+
+app.use((req, res, next) => {
+  res.statusCode = 404;
+  res.write(`
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>Page Not Found</title>
+      </head>
+      <body>
+        <h1>404 kya be jhaat ke baal yhaa kya dekh rha hai </h1>
+      </body>
+      </html>
+    `);
+  res.end();
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`);
+});
