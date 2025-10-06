1e9ad43d4df67ea82eb207651a59c99bcc6d0ffd
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-10-06 19:09:56 +0530

    handling routes for our project air bnb

--- NODE & EXPRESS/1 Airbnb/app.js
+++ NODE & EXPRESS/1 Airbnb/app.js
@@ -7,6 +7,60 @@ const app = express();
 
 app.use(bodyParser.urlencoded());
 
+app.get("/", (req, res, next) => {
+  res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <title>my airbnb</title>
+      </head>
+      <body>
+          <h1>welcome to air bnb</h1>
+          <a href="/add-home">Add Home</a>
+      </body>
+      </html>
+    `);
+})
+
+app.get("/add-home", (req, res, next) => {
+  res.send (`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <title>add home</title>
+      </head>
+      <body>
+          <h1>add Your Home</h1>
+          <form action="/add-home" method="POST">
+              <input type="text"
+              name="houseName"
+              placeholder="Name of your house" />
+              <input type="submit" value="Add Home">
+          </form>
+      </body>
+      </html>
+    `);
+})
+
+app.post("/add-home", (req, res, next) => {
+  console.log(req.body);
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>home added</title>
+    </head>
+    <body>
+        <h1>home added successfully</h1>
+        <a href="/">Go to Home</a>
+    </body>
+    </html>
+    `);
+})
+
 app.use((req, res, next) => {
   console.log('Request Received', req.url, req.method, req.body);
   next();
@@ -23,7 +77,7 @@ app.use((req, res, next) => {
         <title>Page Not Found</title>
       </head>
       <body>
-        <h1>404 kya be jhaat ke baal yhaa kya dekh rha hai </h1>
+        <h1>404 maaf kijiye ap galat page par aa gye hai </h1>
       </body>
       </html>
     `);
